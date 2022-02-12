f3b7d7f5cd8d9c02c614cf8fbc4424a87390754f
Han_B_JeGal <jkhb717@gmail.com>
2022-02-12 20:28:54 +0900

    INITIATIAL COMMIT

---
+++ src/main/java/han_b_jegal/organized/OrganizedApplication.java
@@ -0,0 +1,13 @@
+package han_b_jegal.organized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizedApplication.class, args);
+    }
+
+}
---
+++ src/main/java/han_b_jegal/organized/ServletInitializer.java
@@ -0,0 +1,13 @@
+package han_b_jegal.organized;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OrganizedApplication.class);
+    }
+
+}
